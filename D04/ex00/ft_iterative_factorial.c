be539926f21cdfe36cf8bdfaf84a10b48b84c821
a-lakhani <56142442+a-lakhani@users.noreply.github.com>
2019-10-26 01:06:14 -0700

    D04 ex00

---
+++ D04/ex00/ft_iterative_factorial.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alakhani <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/26 00:10:30 by alakhani          #+#    #+#             */
+/*   Updated: 2019/10/26 01:05:14 by alakhani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_factorial(int nb)
+{
+	int factorial;
+
+	if(nb < 0)
+	{
+		return 0;
+	}
+
+	else if(nb <= 1)
+	{
+		return 1;
+	}
+
+	factorial = nb;
+
+	while(nb > 1)
+	{
+		factorial = factorial * (nb -1);
+		nb--;	
+	}
+
+	return factorial;
+}
