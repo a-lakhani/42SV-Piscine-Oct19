c170513c4c21b03f6a6a961227d9fc23e55811b2
a-lakhani <56142442+a-lakhani@users.noreply.github.com>
2019-10-31 21:55:53 -0700

    strdup

---
+++ D07/ex00/ft_strdup.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alakhani <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/31 21:22:05 by alakhani          #+#    #+#             */
+/*   Updated: 2019/10/31 21:53:14 by alakhani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_strlen(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		len;
+	char	*copy;
+	int		i;
+
+	i = 0;
+	len = ft_strlen(src);
+	copy = NULL;
+	if (len == 0)
+		return (copy);
+	else
+		copy = malloc(len);
+	while (len)
+	{
+		copy[i] = src[i];
+		if (src[i] == '\0')
+			break ;
+		i++;
+	}
+	return (copy);
+}
